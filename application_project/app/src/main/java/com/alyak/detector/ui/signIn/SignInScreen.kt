5626948f12b2ec04d626805b59cc3cd31b74c396
하승재 <21813633@yu.ac.kr>
2025-04-04 00:35:23 +0900

    feature/signup-ui
    signup-ui, signin-ui update

--- application_project/app/src/main/java/com/alyak/detector/ui/signIn/SignInScreen.kt
+++ application_project/app/src/main/java/com/alyak/detector/ui/signIn/SignInScreen.kt
@@ -1,28 +1,28 @@
 package com.alyak.detector.ui.signIn
 
 import android.util.Log
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text2.BasicSecureTextField
-import androidx.compose.foundation.text2.input.TextFieldState
-import androidx.compose.foundation.text2.input.TextObfuscationMode
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.alyak.detector.ui.components.CustomButton
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun SignInScreen() {
     var email by remember { mutableStateOf(" ") }
-    var password by remember { mutableStateOf(false) }
+    var password by remember { mutableStateOf(" ") }
+    var isPasswordVisible by remember { mutableStateOf(false) }
     val state = remember { TextFieldState() }
     //ui layout
     Column(
@@ -32,7 +32,10 @@ fun SignInScreen() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "login", style = MaterialTheme.typography.headlineMedium)
+        Text(
+            text = "login",
+            style = MaterialTheme.typography.headlineMedium
+        )
 
         Spacer(modifier = Modifier.height(16.dp))
 
@@ -47,34 +50,24 @@ fun SignInScreen() {
         Spacer(modifier = Modifier.height(16.dp))
 
         //password input
-        BasicSecureTextField(
-            state = state,
-            textObfuscationMode =
-            if (password) {
-                TextObfuscationMode.Visible
-            } else {
-                TextObfuscationMode.RevealLastTyped
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
 
-            decorator = { innerTextField ->
-                Box(
-                    modifier = Modifier
-                        .align(Alignment.CenterHorizontally)
-                        .padding(start = 16.dp, end = 48.dp)
-                ) {
-                    innerTextField()
-                }
-                androidx.compose.material.icons.Icons(
-                    if (password){
-                        Icons.Filled.
-                    }
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions.Default,
+            trailingIcon = {
+                Icon(
+                    imageVector = if (isPasswordVisible) Icons.Filled.Visibility else Icons.Filled.VisibilityOff,
+                    contentDescription = "Toggle password visibility",
+                    modifier = Modifier.clickable { isPasswordVisible = !isPasswordVisible }
                 )
-
-            }
+            },
+            modifier = Modifier.fillMaxWidth()
         )
+
         Spacer(modifier = Modifier.height(16.dp))
 
         // login Button
@@ -82,7 +75,6 @@ fun SignInScreen() {
             text = "sign in",
             onClick = { Log.d("sign in", "$email , $password") },
             modifier = Modifier
-                .padding(16.dp)
                 .fillMaxWidth()
         )
 
@@ -90,7 +82,6 @@ fun SignInScreen() {
             text = "sign up",
             onClick = { Log.d("sign In", "가입할래우") },
             modifier = Modifier
-                .padding(16.dp)
                 .fillMaxWidth()
         )
     }
