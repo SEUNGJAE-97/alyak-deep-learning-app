4ed18c65f4ffe998074632d69d14866c202ef2f0
seungjae <21813633@yu.ac.kr>
2025-04-07 11:38:37 +0900

    feature/sigin-viewmodel create

--- application_project/app/src/main/java/com/alyak/detector/navigation/NavGraph.kt
+++ application_project/app/src/main/java/com/alyak/detector/navigation/NavGraph.kt
@@ -1,32 +1,39 @@
 package com.alyak.detector.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHost
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.alyak.detector.ui.signIn.SignInScreen
 import com.alyak.detector.ui.signUp.SignUpScreen
+import com.alyak.detector.viewModel.SignInState
+import com.alyak.detector.viewModel.SignInViewModel
 
 @Composable
 fun Navigator() {
     //NavController
     val navController = rememberNavController()
-
+    val signInState : SignInViewModel = viewModel()
     //NavHost
-    NavHost(
+            NavHost(
         navController = navController,
         startDestination = "SignInScreen"
     ) {
-        navigationGraph(navController)
+        navigationGraph(navController, signInState)
     }
 }
 
 //NavGraph
-fun NavGraphBuilder.navigationGraph(navController: NavController) {
+fun NavGraphBuilder.navigationGraph(
+    navController: NavController,
+    signInState : SignInViewModel
+) {
     composable("SignInScreen") {
-        SignInScreen()
+        SignInScreen(navController, signInState)
     }
 
     composable("SignUpScreen") {
