96d6cff25db8f30b58df3d2ef0efd95911d2f8ae
하승재 <21813633@yu.ac.kr>
2025-04-09 00:11:34 +0900

    Create MainScreen.kt
    - 카메라, 복용일정, 알약 리스트 확인

---
+++ application_project/app/src/main/java/com/alyak/detector/ui/main/MainScreen.kt
@@ -0,0 +1,16 @@
+package com.alyak.detector.ui.main
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+//카메라 , 일정, 알약 리스트,
+@Composable
+fun MainScreen() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    MainScreen()
+}
