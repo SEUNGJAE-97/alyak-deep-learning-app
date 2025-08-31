5fb9b81694cbcb9471988ba09260cefc9dfc5c86
하승재 <21813633@yu.ac.kr>
2025-08-31 23:01:12 +0900

    feat : 안드로이드 코드 수정

--- application_project/app/src/main/java/com/alyak/detector/ui/other/StateLabel.kt
+++ application_project/app/src/main/java/com/alyak/detector/ui/other/StateLabel.kt
@@ -7,6 +7,10 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
@@ -18,7 +22,10 @@ import androidx.compose.ui.unit.sp
 import com.alyak.detector.R
 
 @Composable
-fun StateLabel() {
+fun StateLabel(
+    value : String,
+    onValueChange : (String) -> Unit
+) {
     TextField(
         value = value,
         onValueChange = { newValue ->
@@ -49,5 +56,6 @@ fun StateLabel() {
 @Composable
 @Preview(showBackground = true)
 fun StateLabelPreview() {
-    StateLabel()
+    var value by remember { mutableStateOf("") }
+    StateLabel(value = value, onValueChange = {value=it})
 }
