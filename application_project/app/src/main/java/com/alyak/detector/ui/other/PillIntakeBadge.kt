a9edb8940d10d7ad2bc5bbcb5eb5d0fd62a88457
하승재 <sj_hahaha@naver.com>
2025-09-02 16:22:47 +0900

    feat : PillIntakeBadge 추가

---
+++ application_project/app/src/main/java/com/alyak/detector/ui/other/PillIntakeBadge.kt
@@ -0,0 +1,39 @@
+package com.alyak.detector.ui.other
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun PillIntakeBadge(
+    icon: ImageVector,
+) {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(Color.White)
+    ) {
+        Row(
+            modifier = Modifier
+        ) {
+            Icon(
+                modifier = Modifier,
+                imageVector = icon,
+                contentDescription = null,
+            )
+        }
+    }
+}
+
+
+@Composable
+@Preview(showBackground = true)
+fun PillIntakeBadgePreview() {
+    //PillIntakeBadge()
+}
