dd95715998d7a14f304b12f7907e1ae3c16f6c9c
하승재 <21813633@yu.ac.kr>
2025-09-10 01:15:11 +0900

    feat : 가족 리스트 컴포넌트 구현

---
+++ application_project/app/src/main/java/com/alyak/detector/ui/family/HistoryScreen.kt
@@ -0,0 +1,73 @@
+package com.alyak.detector.ui.family
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.alyak.detector.ui.other.BottomForm
+import com.alyak.detector.ui.other.HeaderForm
+
+@Composable
+fun HistoryScreen(
+    modifier: Modifier = Modifier
+) {
+    val icons = listOf(
+        Icons.Filled.Home,
+        Icons.Filled.DateRange,
+        Icons.Filled.FavoriteBorder,
+        Icons.Filled.Settings
+    )
+    var selectedIndex by remember { mutableStateOf(0) }
+
+    Scaffold(
+        topBar = {
+            HeaderForm()
+        },
+        bottomBar = {
+            BottomForm(
+                modifier = Modifier.fillMaxWidth(),
+                icons = icons,
+                selectedIndex = selectedIndex,
+                onItemSelected = { selectedIndex = it }
+            )
+        }
+    ) {
+        paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(paddingValues)
+        ) {
+            // 가족 리스트
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(18.dp),
+                modifier = Modifier.padding(16.dp)
+            ) {
+                FamilyMemberBtn(role = "할머니", name = "김순자", isSelected = true)
+                FamilyMemberBtn(role = "할아버지", name = "하이고", isSelected = false)
+                FamilyMemberBtn(role = "아버지", name = "하태영", isSelected = false)
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun HistoryScreenPrev() {
+    HistoryScreen()
+}
