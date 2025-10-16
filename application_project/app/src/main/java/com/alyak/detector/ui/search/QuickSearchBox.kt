230f4d1dc84cb764f3e8820addb4905fd377fd71
하승재 <sj_hahaha@naver.com>
2025-10-16 17:00:30 +0900

    ui : 랜더링 안되는문제 수정

--- application_project/app/src/main/java/com/alyak/detector/ui/search/QuickSearchBox.kt
+++ application_project/app/src/main/java/com/alyak/detector/ui/search/QuickSearchBox.kt
@@ -6,8 +6,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -30,7 +28,6 @@ fun QuickSearch() {
             .padding(16.dp)
     ) {
         Column(
-            modifier = Modifier.verticalScroll(rememberScrollState()),
             verticalArrangement = Arrangement.spacedBy(10.dp)
         ) {
             Row(
@@ -70,8 +67,6 @@ fun QuickSearch() {
             PillInfoBox(Pill("타이레놀 500mg", "아세트아미노펜", "한국얀센", "일반약", "TYLENOL"))
             PillInfoBox(Pill("타이레놀 500mg", "아세트아미노펜", "한국얀센", "일반약", "TYLENOL"))
             PillInfoBox(Pill("타이레놀 500mg", "아세트아미노펜", "한국얀센", "일반약", "TYLENOL"))
-
-
 
 
         }
