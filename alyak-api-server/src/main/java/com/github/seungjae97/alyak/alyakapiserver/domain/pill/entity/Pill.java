35bc1d794d6ad05c1c9c7f642c12f403216ee206
하승재 <sj_hahaha@naver.com>
2025-11-14 15:38:44 +0900

    feat : pill 관련 entity 생성

---
+++ alyak-api-server/src/main/java/com/github/seungjae97/alyak/alyakapiserver/domain/pill/entity/Pill.java
@@ -0,0 +1,53 @@
+package com.github.seungjae97.alyak.alyakapiserver.domain.pill.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "pill")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Pill {
+
+    @Id
+    @Column(name = "pill_id")
+    private Long id;
+
+    @Column(name = "pill_name")
+    private String pillName;
+
+    @Column(name = "pill_description")
+    private String pillDescription;
+
+    @Column(name = "user_method")
+    private String userMethod;
+
+    @Column(name = "pill_efficacy")
+    private String pillEfficacy;
+
+    @Column(name = "pill_warn")
+    private String pillWarn;
+
+    @Column(name = "pill_caution")
+    private String pillCaution;
+
+    @Column(name = "pill_interactive")
+    private String pillInteractive;
+
+    @Column(name = "pill_adverse_reaction")
+    private String pillAdverseReaction;
+
+    @Column(name = "pill_manufacturer")
+    private String pillManufacturer;
+
+    @Column(name = "pill_img")
+    private String pillImg;
+
+}
---
+++ alyak-api-server/src/main/java/com/github/seungjae97/alyak/alyakapiserver/domain/pill/entity/PillAppearance.java
@@ -0,0 +1,49 @@
+package com.github.seungjae97.alyak.alyakapiserver.domain.pill.entity;
+
+import com.github.seungjae97.alyak.alyakapiserver.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "pill_appearance")
+public class PillAppearance {
+
+    @Id
+    @Column(name = "pill_id")
+    private Long pillId;
+
+    @Column(name = "pill_front")
+    private String pillFront;
+
+    @Column(name = "pill_back")
+    private String pillBack;
+
+    @Column(name = "pill_classification")
+    private String pillClassification;
+
+    @Column(name = "pill_type")
+    private String pillType;
+
+    @Column(name = "shape_id")
+    private Long shapeId;
+
+    @Column(name = "color_id")
+    private Long colorId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "shape_id", insertable = false, updatable = false)
+    private PillShape pillShapeId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "color_id", insertable = false, updatable = false)
+    private PillColor pillColorId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId("pillId")
+    @JoinColumn(name = "pill_id", nullable = false)
+    private Pill pill;
+}
